a33e8449da51b35f40f06cf7de9763263b9173bd
Ganga Ram <131853076+gngram@users.noreply.github.com>
2025-08-26 17:20:29 +0400

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -4,13 +4,21 @@ pkgs.mkShell {
   packages = [
     pkgs.python311
     pkgs.python311Packages.pyqt5
+    pkgs.python311Packages.virtualenv
     pkgs.qt5.qtwayland
     pkgs.python311Packages.setuptools
     pkgs.python311Packages.wheel
     pkgs.python311Packages.build
   ];
   shellHook = ''
-    export QT_QPA_PLATFORM=${QT_QPA_PLATFORM:-wayland}
+    if [ ! -d .venv ]; then
+      virtualenv .venv
+      source .venv/bin/activate
+    else
+      source .venv/bin/activate
+    fi
+    echo "Welcome to your Python development environment."
+    export QT_QPA_PLATFORM=wayland
     echo "Now you can:"
     echo "  pip install -e ."
     echo "  devicerouter-gui --test-file ./schema.json"
